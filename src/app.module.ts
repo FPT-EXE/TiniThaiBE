8322e8a30a2c7298e5cff99783adf49d7a4d5efc
Daniel Ng <dacnguyen9101@gmail.com>
2023-05-23 08:59:54 +0700

    feat: gen users, exercises

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { UsersModule } from './users/users.module';
+import { ExercisesModule } from './exercises/exercises.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, UsersModule, ExercisesModule],
   controllers: [AppController],
   providers: [AppService],
 })
