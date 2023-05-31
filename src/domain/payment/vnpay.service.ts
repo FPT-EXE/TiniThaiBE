62383a0e26e5254f214c9365aab6302eec32e6d0
Daniel Ng <dacnguyen9101@gmail.com>
2023-06-01 01:11:20 +0700

    chore: enable esModuleInterop

--- src/domain/payment/vnpay.service.ts
+++ src/domain/payment/vnpay.service.ts
@@ -1,7 +1,7 @@
-import * as crypto from 'crypto';
+import crypto from 'crypto';
 
 import { Injectable } from '@nestjs/common';
-import * as qs from 'qs';
+import qs from 'qs';
 
 import { BootConfigService } from '../../application/configuration/boot.config';
 import {
@@ -11,23 +11,23 @@ import {
 } from '../../application/utils';
 
 
-type VnPayParams = {
-	vnp_Version: string,
-	vnp_Command: string,
-	vnp_TmnCode: string,
-	vnp_Amount: string,
-	vnp_CreateDate: string,
-	vnp_CurrCode: string,
-	vnp_IpAddr: string,
-	vnp_Locale: string,
-	vnp_OrderInfo: string,
-	vnp_ReturnUrl: string,
-	vnp_TxnRef: string,
+type TransactionParams = {
+	vnp_Version    : '2.0.1' | '2.1.0',
+	vnp_Command    : 'pay',
+	vnp_Locale     : 'vn',
+	vnp_CurrCode   : 'VND',
+	vnp_TmnCode    : string,
+	vnp_Amount     : string,
+	vnp_CreateDate : string,
+	vnp_IpAddr     : string,
+	vnp_OrderInfo  : string,
+	vnp_ReturnUrl  : string,
+	vnp_TxnRef     : string,
 };
 
-type VnPayParamsHash = VnPayParams & { vnp_SecureHash: string };
+type VnPayParams = TransactionParams & { vnp_SecureHash: string };
 
-type Payment = {
+type CreatePaymentParam = {
 	amount: number,
 	ipAddress: string,
 };
@@ -36,45 +36,42 @@ type Payment = {
 export class VnPayService {
 	constructor(private readonly _configSvc: BootConfigService) {}
 
-	public createPaymentUrl({ amount, ipAddress }: Payment): string {
-		const vnpUrl = this._configSvc.VNP_URL;
-		const returnUrl = this._configSvc.VNP_RETURN_URL;
-		const tmnCode = this._configSvc.VNP_TMNCODE;
-		const secretKey = this._configSvc.VNP_HASHSECRET;
-		const locale = 'vn';
-		const current = new Date();
-		const vnpCreateDate = formatYYYYMMDDHHMMSS(current);
-		const orderId = formatHHMMSS(current);
-		const orderInfo = 'Payment:' + orderId;
-		const vnpVersion = '2.1.0';
-		const vnpCommand = 'pay';
-		const vnpAmount = `${amount * 100}`;
-		const currCode = 'VND';
-
-		let vnpParams: VnPayParams = {
-			vnp_Version: vnpVersion,
-			vnp_Command: vnpCommand,
-			vnp_TmnCode: tmnCode,
-			vnp_Amount: vnpAmount,
-			vnp_CreateDate: vnpCreateDate,
-			vnp_CurrCode: currCode,
-			vnp_IpAddr: ipAddress,
-			vnp_Locale: locale,
-			vnp_OrderInfo: orderInfo,
-			vnp_ReturnUrl: returnUrl,
-			vnp_TxnRef: orderId,
-		};
-		vnpParams = sortObject(vnpParams);
-
-		const signData = qs.stringify(vnpParams, { encode: false });
-		const hmac = crypto.createHmac('sha512', secretKey);
-		const signed = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
-		const hashingVnpParams: VnPayParamsHash = {
-			vnp_SecureHash: signed,
-			...vnpParams,
+	public createPaymentUrl({ amount, ipAddress }: CreatePaymentParam): string {
+		const vnpUrl              = this._configSvc.VNP_URL;
+		const secretKey           = this._configSvc.VNP_HASHSECRET;
+		const transactionParams   = sortObject(this._createTransactionParams(amount, ipAddress));
+		
+		const vnpParams: VnPayParams = {
+			vnp_SecureHash: this._signHashKey(transactionParams, secretKey),
+			...transactionParams,
 		};
 
-		const paymentUrl = vnpUrl + '?' + qs.stringify(hashingVnpParams, { encode: false });
+		const paymentUrl = vnpUrl + '?' + qs.stringify(vnpParams, { encode: false });
 		return paymentUrl;
+	}
+
+	private _signHashKey(transactionParams: TransactionParams,secretKey: string): string {
+		const signData    = qs.stringify(transactionParams, { encode: false });
+		const hmac        = crypto.createHmac('sha512', secretKey);
+		const signed      = hmac.update(Buffer.from(signData, 'utf-8')).digest('hex');
+		return signed;
+	}
+
+	private _createTransactionParams(amount: number, ipAddress: string): TransactionParams {
+		const current = new Date();
+		const orderId = formatHHMMSS(current);
+		return {
+			vnp_Version    : '2.1.0',
+			vnp_Command    : 'pay',
+			vnp_Locale     : 'vn',
+			vnp_CurrCode   : 'VND',
+			vnp_IpAddr     : ipAddress,
+			vnp_TxnRef     : orderId,
+			vnp_OrderInfo  : 'Payment:' + orderId,
+			vnp_Amount     : String(amount * 100),
+			vnp_CreateDate : formatYYYYMMDDHHMMSS(current),
+			vnp_TmnCode    : this._configSvc.VNP_TMNCODE,
+			vnp_ReturnUrl  : this._configSvc.VNP_RETURN_URL,
+		};
 	}
 }
